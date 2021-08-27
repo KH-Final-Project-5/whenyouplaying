7895a60186e7df3330dfbb10ec4d0d65d4dc4f71
EjinS <gooejin21@gmail.com>
2021-08-27 19:22:34 +0900

    [create] 거래페이지, 이미지 dto 추가

---
+++ src/main/java/com/dto/DealStatus.java
@@ -0,0 +1,20 @@
+package com.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DealStatus {
+
+	private int dealNo;
+	private int prNo;
+	private int usSellNo;
+	private int usBuyNo;
+	
+	
+}
---
+++ src/main/java/com/dto/DealStatusImg.java
@@ -0,0 +1,18 @@
+package com.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DealStatusImg {
+
+	private int dealImgNo;
+	private String dealImgSrc;
+	private String dealImgContent;
+	
+}
