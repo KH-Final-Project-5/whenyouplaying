1d2a3bb034b10d377c3d23b21943d33863cd0eb4
KimWanJoo <wjwan0915@gmail.com>
2021-09-12 23:06:40 +0900

    [CREATE] 채팅 1차구현

---
+++ src/main/java/com/dto/ChatSeller.java
@@ -0,0 +1,16 @@
+package com.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ChatSeller {
+
+    private String writer;
+    private String buyer;
+    private int dealNo;
+    private String content;
+}
